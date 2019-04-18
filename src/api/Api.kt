6e002e09e3ced25d52f1dd85e148cc561470a125
Sean Shubin <sshubin@cj.com>
2019-04-18 16:15:38 -0700

    simplified api

--- src/api/Api.kt
+++ src/api/Api.kt
@@ -1,30 +1,27 @@
 package api
 
 import model.Ballot
+import model.Credentials
 import model.Election
 import kotlin.js.Promise
 
 interface Api {
-    fun login(nameOrEmail: String, password: String): Promise<Response<Unit>>
-    fun logout(): Promise<Response<Unit>>
-    fun register(name: String, email: String, password: String): Promise<Response<Unit>>
-    fun createElection(electionName: String): Promise<Response<Unit>>
-    fun copyElection(newElectionName: String, electionToCopyName: String): Promise<Response<Unit>>
-    fun listElections(): Promise<Response<List<Election>>>
-    fun getElection(electionName: String): Promise<Response<Election>>
-    fun doneEditingElection(electionName: String): Promise<Response<Unit>>
-    fun startElection(electionName: String): Promise<Response<Unit>>
-    fun endElection(electionName: String): Promise<Response<Unit>>
-    fun listCandidates(electionName: String): Promise<Response<List<String>>>
-    fun updateCandidates(electionName: String, candidates: List<String>): Promise<Response<Unit>>
-    fun listEligibleVoters(electionName: String): Promise<Response<List<String>>>
-    fun listAllVoters(): Promise<Response<List<String>>>
-    fun updateEligibleVoters(electionName: String, eligibleVoters: List<String>): Promise<Response<Unit>>
-    fun listBallots(voterName: String): Promise<Response<List<Ballot>>>
-    fun getBallot(electionName: String, voterName: String): Promise<Response<Ballot>>
-    fun castBallot(electionName: String, voterName: String, ballot: Ballot): Promise<Response<Unit>>
-
-    interface Response<T>
-    data class Success<T>(val value: T) : Response<T>
-    data class Failure<T>(val message: String) : Response<T>
+    fun login(nameOrEmail: String, password: String): Promise<Credentials>
+    fun logout(): Promise<Unit>
+    fun register(name: String, email: String, password: String): Promise<Credentials>
+    fun createElection(credentials: Credentials, electionName: String): Promise<Unit>
+    fun copyElection(credentials: Credentials, newElectionName: String, electionToCopyName: String): Promise<Unit>
+    fun listElections(credentials: Credentials): Promise<List<Election>>
+    fun getElection(credentials: Credentials, electionName: String): Promise<Election>
+    fun doneEditingElection(credentials: Credentials, electionName: String): Promise<Unit>
+    fun startElection(credentials: Credentials, electionName: String): Promise<Unit>
+    fun endElection(credentials: Credentials, electionName: String): Promise<Unit>
+    fun listCandidates(credentials: Credentials, electionName: String): Promise<List<String>>
+    fun updateCandidates(credentials: Credentials, electionName: String, candidates: List<String>): Promise<Unit>
+    fun listEligibleVoters(credentials: Credentials, electionName: String): Promise<List<String>>
+    fun listAllVoters(credentials: Credentials): Promise<List<String>>
+    fun updateEligibleVoters(credentials: Credentials, electionName: String, eligibleVoters: List<String>): Promise<Unit>
+    fun listBallots(credentials: Credentials, voterName: String): Promise<List<Ballot>>
+    fun getBallot(credentials: Credentials, electionName: String, voterName: String): Promise<Ballot>
+    fun castBallot(credentials: Credentials, electionName: String, voterName: String, ballot: Ballot): Promise<Unit>
 }
--- src/api/ApiFake.kt
+++ src/api/ApiFake.kt
@@ -1,92 +1,116 @@
 package api
 
 import model.Ballot
+import model.Credentials
 import model.Election
 import model.User
 import kotlin.js.Promise
 
 class ApiFake : Api {
     private val users: MutableList<User> = mutableListOf()
-    override fun login(nameOrEmail: String, password: String): Promise<Api.Response<Unit>> {
+    override fun login(nameOrEmail: String, password: String): Promise<Credentials> {
         val user = findUser(nameOrEmail)
         return when {
             user == null -> loginError(nameOrEmail)
-            user.password == password -> Promise.resolve(Api.Success<Unit>(Unit))
+            user.password == password -> Promise.resolve(Credentials(user.name, user.password))
             else -> loginError(nameOrEmail)
         }
     }
 
-    override fun logout(): Promise<Api.Response<Unit>> {
+    override fun logout(): Promise<Unit> = Promise.resolve(Unit)
+
+    override fun register(name: String, email: String, password: String): Promise<Credentials> {
+        return try {
+            assertUserNameDoesNotExist(name)
+            assertUserEmailDoesNotExist(email)
+            val user = createUser(name, email, password)
+            Promise.resolve(Credentials(user.name, user.password))
+        } catch (ex: RuntimeException) {
+            Promise.reject(ex)
+        }
+    }
+
+    override fun createElection(credentials: Credentials, electionName: String): Promise<Unit> {
         TODO("not implemented")
     }
 
-    override fun register(name: String, email: String, password: String): Promise<Api.Response<Unit>> {
+    override fun copyElection(credentials: Credentials, newElectionName: String, electionToCopyName: String): Promise<Unit> {
         TODO("not implemented")
     }
 
-    override fun createElection(electionName: String): Promise<Api.Response<Unit>> {
+    override fun listElections(credentials: Credentials): Promise<List<Election>> {
         TODO("not implemented")
     }
 
-    override fun copyElection(newElectionName: String, electionToCopyName: String): Promise<Api.Response<Unit>> {
+    override fun getElection(credentials: Credentials, electionName: String): Promise<Election> {
         TODO("not implemented")
     }
 
-    override fun listElections(): Promise<Api.Response<List<Election>>> {
+    override fun doneEditingElection(credentials: Credentials, electionName: String): Promise<Unit> {
         TODO("not implemented")
     }
 
-    override fun getElection(electionName: String): Promise<Api.Response<Election>> {
+    override fun startElection(credentials: Credentials, electionName: String): Promise<Unit> {
         TODO("not implemented")
     }
 
-    override fun doneEditingElection(electionName: String): Promise<Api.Response<Unit>> {
+    override fun endElection(credentials: Credentials, electionName: String): Promise<Unit> {
         TODO("not implemented")
     }
 
-    override fun startElection(electionName: String): Promise<Api.Response<Unit>> {
+    override fun listCandidates(credentials: Credentials, electionName: String): Promise<List<String>> {
         TODO("not implemented")
     }
 
-    override fun endElection(electionName: String): Promise<Api.Response<Unit>> {
+    override fun updateCandidates(credentials: Credentials, electionName: String, candidates: List<String>): Promise<Unit> {
         TODO("not implemented")
     }
 
-    override fun listCandidates(electionName: String): Promise<Api.Response<List<String>>> {
+    override fun listEligibleVoters(credentials: Credentials, electionName: String): Promise<List<String>> {
         TODO("not implemented")
     }
 
-    override fun updateCandidates(electionName: String, candidates: List<String>): Promise<Api.Response<Unit>> {
+    override fun listAllVoters(credentials: Credentials): Promise<List<String>> {
         TODO("not implemented")
     }
 
-    override fun listEligibleVoters(electionName: String): Promise<Api.Response<List<String>>> {
+    override fun updateEligibleVoters(credentials: Credentials, electionName: String, eligibleVoters: List<String>): Promise<Unit> {
         TODO("not implemented")
     }
 
-    override fun listAllVoters(): Promise<Api.Response<List<String>>> {
+    override fun listBallots(credentials: Credentials, voterName: String): Promise<List<Ballot>> {
         TODO("not implemented")
     }
 
-    override fun updateEligibleVoters(electionName: String, eligibleVoters: List<String>): Promise<Api.Response<Unit>> {
+    override fun getBallot(credentials: Credentials, electionName: String, voterName: String): Promise<Ballot> {
         TODO("not implemented")
     }
 
-    override fun listBallots(voterName: String): Promise<Api.Response<List<Ballot>>> {
-        TODO("not implemented")
-    }
-
-    override fun getBallot(electionName: String, voterName: String): Promise<Api.Response<Ballot>> {
-        TODO("not implemented")
-    }
-
-    override fun castBallot(electionName: String, voterName: String, ballot: Ballot): Promise<Api.Response<Unit>> {
+    override fun castBallot(credentials: Credentials, electionName: String, voterName: String, ballot: Ballot): Promise<Unit> {
         TODO("not implemented")
     }
 
     private fun findUser(nameOrEmail: String): User? =
             users.find { user -> user.name == nameOrEmail || user.email == nameOrEmail }
 
-    private fun loginError(nameOrEmail: String): Promise<Api.Response<Unit>> =
-            Promise.resolve(Api.Failure("Unable to authenticate user '$nameOrEmail'"))
+    private fun loginError(nameOrEmail: String): Promise<Credentials> =
+            Promise.reject(RuntimeException("Unable to authenticate user '$nameOrEmail'"))
+
+    private fun assertUserNameDoesNotExist(name: String) {
+        if (users.find { it.name == name } != null) {
+            throw RuntimeException("User named '$name' already exists")
+        }
+    }
+
+    private fun assertUserEmailDoesNotExist(email: String) {
+        if (users.find { it.email == email } != null) {
+            throw RuntimeException("User with email '$email' already exists")
+        }
+    }
+
+    private fun createUser(name: String, email: String, password: String): User {
+        val user = User(name, email, password)
+        users.add(user)
+        return user
+    }
 }
