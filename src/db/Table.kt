ecc4b1de59c87a719e2d047cdd0e8ffd354d6437
SeanShubin <seanshubin@gmail.com>
2019-05-28 13:08:09 -0700

    db prototyping

--- src/db/Table.kt
+++ src/db/Table.kt
@@ -1,9 +1,22 @@
 package db
 
 interface Table<PkType, T : HasPrimaryKey<PkType>> {
+    fun search(key:PkType):T?
+    fun listAll():List<T>
     fun add(value: T)
-    fun list(): List<T>
-    fun remove(key: PkType)
     fun update(value: T)
-    fun addOrUpdate(value: T)
+    fun remove(key: PkType)
+
+    fun listWhere(p:(T)->Boolean): List<T> = listAll().filter(p)
+    fun existsWhere(p:(T)->Boolean):Boolean = listWhere(p).isNotEmpty()
+    fun find(key:PkType):T = search(key)!!
+    fun keyExists(key:PkType):Boolean = search(key) != null
+
+
+//    fun add(values: List<T>)
+//    fun countAll(): Int
+//    fun countWhere(p:(T)->Boolean): Int
+//    fun removeAllByKeys(keys:List<PkType>)
+//    fun removeAllByValues(values:List<T>)
+//    fun addOrUpdate(value: T)
 }
